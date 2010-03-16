2b5c980ff195f73504b9b9338a71892633f7589e
robmckinnon <rob@theyworkforyou.co.nz>
2010-03-16 11:00:53 +0000

    refactoring common pdf parser logic into eu_cohesion_base

--- parsers/eu_cohesion/de_berlin_parse.rb
+++ parsers/eu_cohesion/de_berlin_parse.rb
@@ -1,30 +1,67 @@
 require File.expand_path(File.dirname(__FILE__) + '/eu_cohesion_base')
 
-class EuCohesion::Receiver
-  # def method_missing symbol, *args
-    # puts symbol.to_s
-  # end
-end
-
-class EuCohesion::Cell
-  include Morph
-end
-
 class EuCohesion::DeBerlinParse
 
   include EuCohesion::ParserBase
 
-  def has_string_value? text
-    text && text.respond_to?(:value) && text.value.is_a?(String)
+  def perform result
+    resources = result.scraped_resources
+    resource = resources.first
+    parse resource
+  end
+  
+  def parse resource
+    formats = [:string, :string, /^\d\d\.\d\d\.\d\d\d\d$/, /^(\d|\.)*\,\d\d$/, /^(\d|\.)*\,\d\d$/]    
+    groups = get_text_groups(resource, formats)
+    first_values = values_at_position groups, 0
+    @projects = []
+
+    groups.each do |group|
+      project = EuCohesion::Project.new
+      by_position = group.group_by {|x| x.left.to_i }
+
+      if by_position.keys.size == 4
+        by_position.keys.inspect + ': ' + group.inspect + ' -> ' + by_position.inspect
+        first_value = group.first.value
+        first_values.each do |value|
+          if first_value[/^#{value} (.+)$/]
+            text = group.first
+            text.value = value
+            cell = make_cell text.morph_attributes, $1, 1
+            group.insert(1, cell)
+          end
+        end
+        by_position = group.group_by {|x| x.left.to_i }
+      end
+
+      if by_position.keys.size != 5
+        raise by_position.keys.inspect + ': ' + group.inspect + ' -> ' + by_position.inspect
+      end
+
+      project = EuCohesion::Project.new
+      by_position.keys.sort.each_with_index do |key, index|
+        value = by_position[key].collect(&:value).join(' ').squeeze(' ')
+        unless @pdf_text.include?(value) || @plain_pdf_text.include?(value)
+          if value[/\d\d\.\d\d\.\d\d\d\d/]
+            raise "#{value} not found in pdf text"
+          end
+        end
+        project.morph(attribute_keys[index], value)
+      end
+      @projects << project
+    end
+    
+    write_csv attribute_keys, attribute_keys, 'eu_cohesion/de_berlin.csv'
   end
 
-  def format_match offset
-    text = @stack[offset]
-    has_string_value?(text) && text.value[@formats[offset]]
+  def first_data_value 
+    '1. Frauenfußball Verein Spandau e.V.'
   end
 
-  def start_of_data text
-    !@started && has_string_value?(text) && (text.value == '1. Frauenfußball Verein Spandau e.V.')
+  def ignore_this
+    {
+      'Begünstigtenverzeichnis der Operationellen Programms des EFRE in Berlin 2007-2013 (Stand 31.12.2008)' => true
+    }
   end
 
   def split_this
@@ -69,144 +106,6 @@ class EuCohesion::DeBerlinParse
         ['UNIONHILFSWERK Sozialeinrichtungen gGmbH',
         'Kindertagesstätte Neukölln, Weserstraße 185, 12045 Berlin']
     }
-  end
-
-  def make_cell attributes, text, delta
-    cell = EuCohesion::Cell.new
-    cell.morph attributes
-    cell.morph('value', text)
-    if delta > 0
-      cell.left = (cell.left.to_i + delta).to_s
-    end
-    cell
-  end
-  
-  def get_texts result
-    resources = result.scraped_resources
-    @pdf_text = resources.first.contents
-    @plain_pdf_text = resources.first.plain_pdf_contents
-    xml = resources.first.xml_pdf_contents.gsub(" id="," id_attr=")
-    doc = Hpricot.XML xml
-        
-    (doc/'text').collect do |text|
-      attributes = text.attributes.to_hash
-      text = text.inner_text
-      if parts = split_this[text]
-        parts.collect { |part| make_cell(attributes, part, parts.index(part)) }
-      else
-        make_cell(attributes, text, 0)
-      end
-    end.flatten
-  end
-
-  def group_text text
-    @started = true if start_of_data(text)
-    if @started
-      if has_string_value?(text)
-        @stack << text unless text.value == 'Begünstigtenverzeichnis der Operationellen Programms des EFRE in Berlin 2007-2013 (Stand 31.12.2008)'
-      else
-        $stderr.write text.inspect
-      end
-      if format_match(-1) && format_match(-2) && format_match(-3)
-        @groups << @stack
-        @stack = []
-      end
-    end
-  end
-  
-  def perform result
-    @formats = [:string, :string, /^\d\d\.\d\d\.\d\d\d\d$/, /^(\d|\.)*\,\d\d$/, /^(\d|\.)*\,\d\d$/]    
-    @groups = []
-    @stack = []
-    @started = false
-    texts = get_texts(result)
-
-    output = FasterCSV.generate do |csv|
-      texts.each do |text|
-        csv << text.value
-      end
-    end
-    File.open('texts.csv','w') {|f| f.write output}
-
-    texts.each do |text|
-      group_text text
-    end
-    
-    output = FasterCSV.generate do |csv|
-      @groups.each do |texts|
-        csv << texts.collect(&:value)
-      end
-    end
-    File.open('groups.csv','w') {|f| f.write output}
-
-    @projects = []
-    
-    # y @groups
-    
-    first_values = @groups.collect(&:first).collect(&:value).uniq.collect { |v| v.gsub('+','\+').gsub('(','\(').gsub(')','\)').gsub('[','\[').gsub(']','\]') }
-
-    @groups.each do |group|
-      project = EuCohesion::Project.new
-      raise group.inspect if group.select{|x|x.is_a?(Hash)}.size > 0
-
-      by_position = group.group_by {|x| x.left.to_i }
-
-      if by_position.keys.size == 4
-        by_position.keys.inspect + ': ' + group.inspect + ' -> ' + by_position.inspect
-
-        first_value = group.first.value
-        first_values.each do |value|
-          if first_value[/^#{value} (.+)$/]
-            text = group.first
-            text.value = value
-            cell = make_cell text.morph_attributes, $1, 1
-            group.insert(1, cell)
-          end
-        end
-        by_position = group.group_by {|x| x.left.to_i }
-      end
-
-      if by_position.keys.size != 5
-        raise by_position.keys.inspect + ': ' + group.inspect + ' -> ' + by_position.inspect
-      end
-      
-      project = EuCohesion::Project.new
-      by_position.keys.sort.each_with_index do |key, index|
-        value = by_position[key].collect(&:value).join(' ').squeeze(' ')
-        unless @pdf_text.include?(value) || @plain_pdf_text.include?(value)
-          if value[/\d\d\.\d\d\.\d\d\d\d/]
-            raise "#{value} not found in pdf text"
-          end
-        end
-        project.morph(attribute_keys[index], value)
-      end
-      @projects << project
-    end
-    
-    
-    # puts resources.first.web_resource.file_path
-    # puts resources.first.web_resource.file_path
-    
-    # receiver = EuCohesion::Receiver.new    
-    # PDF::Reader.file(resources.first.web_resource.file_path, receiver)
-
-    # parse text
-# 
-    # y @projects
-
-    write_csv attribute_keys, attribute_keys, 'eu_cohesion/de_berlin.csv'
-  end
-  
-  def handle_page
-  end
-  
-  def parse text
-    text.each_line do |line|
-      case line
-        when /^[^\s]/
-          add_project(line) {|data, project| values_from_line(data)}
-      end
-    end
   end
 
   def attribute_keys
--- parsers/eu_cohesion/eu_cohesion_base.rb
+++ parsers/eu_cohesion/eu_cohesion_base.rb
@@ -3,6 +3,10 @@ require 'pdf/reader'
 module EuCohesion
 end
 
+class EuCohesion::Cell
+  include Morph
+end
+
 class EuCohesion::Project
   include Morph
 end
@@ -35,4 +39,83 @@ module EuCohesion::ParserBase
     end
     @projects << project
   end
+
+  def make_cell attributes, text, delta
+    cell = EuCohesion::Cell.new
+    cell.morph attributes
+    cell.morph('value', text)
+    if delta > 0
+      cell.left = (cell.left.to_i + delta).to_s
+    end
+    cell
+  end
+  
+  def has_string_value? text
+    text && text.respond_to?(:value) && text.value.is_a?(String)
+  end
+
+  def format_match offset
+    text = @stack[offset]
+    has_string_value?(text) && text.value[@formats[offset]]
+  end
+
+  def start_of_data text
+    !@started && has_string_value?(text) && (text.value == first_data_value)
+  end
+
+  def group_text text
+    @started = true if start_of_data(text)
+    if @started
+      if has_string_value?(text)
+        @stack << text unless ignore_this[text.value]
+      else
+        $stderr.write text.inspect
+      end
+      if format_match(-1) && format_match(-2) && format_match(-3)
+        @groups << @stack
+        @stack = []
+      end
+    end
+  end
+  
+  def get_text_groups resource, formats
+    @formats = formats
+    @stack = []
+    @started = false
+    @groups = []
+    @pdf_text = resource.contents
+    @plain_pdf_text = resource.plain_pdf_contents
+    xml = resource.xml_pdf_contents.gsub(" id="," id_attr=")
+    doc = Hpricot.XML xml
+        
+    texts = (doc/'text').collect do |text|
+      attributes = text.attributes.to_hash
+      text = text.inner_text
+      if parts = split_this[text]
+        parts.collect { |part| make_cell(attributes, part, parts.index(part)) }
+      else
+        make_cell(attributes, text, 0)
+      end
+    end.flatten
+    output = FasterCSV.generate do |csv|
+      texts.each {|text| csv << text.value }
+    end
+    File.open('texts.csv','w') {|f| f.write output}
+    texts.each {|text| group_text(text) }
+    
+    output = FasterCSV.generate do |csv|
+      @groups.each {|texts| csv << texts.collect(&:value) }
+    end
+    File.open('groups.csv','w') {|f| f.write output}
+    
+    @groups.each do |group|
+      raise group.inspect if group.select{|x|x.is_a?(Hash)}.size > 0 # check groups not hashes
+    end
+    @groups
+  end
+  
+  def values_at_position groups, index
+    groups.collect{|g| g[index]}.collect(&:value).uniq.collect { |v| v.gsub('+','\+').gsub('(','\(').gsub(')','\)').gsub('[','\[').gsub(']','\]') }
+  end
+
 end
